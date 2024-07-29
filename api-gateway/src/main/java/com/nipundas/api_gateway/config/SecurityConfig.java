f79be09958b314bbfe489783113d00cee1cd26c7
nipun-das <nipundas02@gmail.com>
2024-07-29 22:03:38 +0530

    added keycloak authentication

--- api-gateway/src/main/java/com/nipundas/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/nipundas/api_gateway/config/SecurityConfig.java
@@ -8,11 +8,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
+
     }
 }
