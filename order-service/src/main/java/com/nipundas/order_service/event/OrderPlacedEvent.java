8bd944137a767b17c8e7a24500139892dd31c208
nipun-das <nipundas02@gmail.com>
2024-08-24 14:29:15 +0530

    orderplaced event created

--- order-service/src/main/java/com/nipundas/order_service/event/OrderPlacedEvent.java
+++ order-service/src/main/java/com/nipundas/order_service/event/OrderPlacedEvent.java
@@ -1,12 +1,14 @@
 package com.nipundas.order_service.event;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderPlacedEvent {
-    public void jjjj(){
-//sdfsdfsdfs
-
-
-
-
-        
-    }
+    private String orderNumber;
+    private String email;
 }
