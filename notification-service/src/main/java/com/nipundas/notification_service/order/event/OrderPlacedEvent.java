ea79344058ee183cea7828a48642d9bad3685516
nipun-das <nipundas02@gmail.com>
2024-08-24 22:13:25 +0530

    added mailtrap.io configs

---
+++ notification-service/src/main/java/com/nipundas/notification_service/order/event/OrderPlacedEvent.java
@@ -0,0 +1,13 @@
+package com.nipundas.notification_service.order.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+    private String email;
+}
